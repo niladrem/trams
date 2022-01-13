e1afbf3e576da6b1a4172f2ac1d368ca6745b8b4
niladrem <aurbala@gmail.com>
2022-01-13 20:43:02 +0100

    scripts

---
+++ preparation.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# katalogi na tabele externalne i dump danych
+hdfs dfs -mkdir /data
+hdfs dfs -mkdir /data/in
+hdfs dfs -mkdir /data/trams
+hdfs dfs -mkdir /data/stops
+
+# stworzenie tabel (skrypt w extra)
+hive -f create_table.hql
+
+# certy do API
+keytool -import -alias nificert -file dvcasha2.cer -keystore truststore.jks -storepass $PASS
+
+# stworzenie topiców
+/usr/local/kafka/bin/kafka-topics.sh --create --topic input-trams --bootstrap-server localhost:9092 --partitions 1 --replication-factor 1
+/usr/local/kafka/bin/kafka-topics.sh --create --topic output-trams --bootstrap-server localhost:9092 --partitions 1 --replication-factor 1
+/usr/local/kafka/bin/kafka-topics.sh --create --topic fast-trams --bootstrap-server localhost:9092 --partitions 1 --replication-factor 1
+
+# deploy joba flinkowego (config w extra)
+sudo /usr/local/flink/bin/start-cluster.sh
+sudo /usr/local/flink/bin/flink run -c pw.mini.DeduplicationJob /home/vagrant/trams/deduplication-1.0-SNAPSHOT.jar /home/vagrant/trams/config.properties
